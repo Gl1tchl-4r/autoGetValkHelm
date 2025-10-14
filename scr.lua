0115ba6f4631eae1df00fd0a944db07ac720dde5
Gl1tch <128307423+Gl1tchl-4r@users.noreply.github.com>
2025-10-14 16:50:30 +0700

    Update scr.lua

--- scr.lua
+++ scr.lua
@@ -184,7 +184,7 @@ local function fastAttack()
                                 local mt = getrawmetatable(cached._Object)
                                 setreadonly(mt, false)
                                 local oldIndex = mt.__index
-                                mt.__index = function(t, k) return k == "Length" and 0.13 end
+                                mt.__index = function(t, k) return k == "Length" and 0.13
                                 setreadonly(mt, true)
                             end)
                         end
